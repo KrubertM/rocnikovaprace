5368fb306a8760c2eaa38e176604ae6704222709
KrubertM <115136586+KrubertM@users.noreply.github.com>
2024-08-10 15:46:01 +0200

    Update ufo.py

--- ufo.py
+++ ufo.py
@@ -1,15 +1,1 @@
-import pygame
 
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Enemy, self).__init__()
-        self.image = pygame.image.load("Ročníková_Práce/img/ufo.png").convert_alpha()
-        self.rect = self.image.get_rect()
-        self.x = 0
-        self.y = 0
-        self.speed = 5
-        
-        
-    def update(self):
-        self.rect.x += self.x
-        self.rect.y += self.y
