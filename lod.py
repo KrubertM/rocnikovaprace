20040a3f2adeab9b021d349cf846da532233d625
KrubertM <115136586+KrubertM@users.noreply.github.com>
2024-08-10 15:45:42 +0200

    Update lod.py

--- lod.py
+++ lod.py
@@ -1,15 +1,1 @@
-import pygame
 
-class Ship(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Ship, self).__init__()
-        self.image = pygame.image.load("Ročníková_Práce/img/ship.png").convert_alpha()
-        self.rect = self.image.get_rect()
-        self.x = 350
-        self.y = 700
-        self.speed = 5
-        
-        
-    def update(self):
-        self.rect.x += self.x
-        self.rect.y += self.y
